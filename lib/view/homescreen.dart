720ffc0b5c10a988f3847dec2ab9d246c64ea900
MEetpanchal1222 <panchalmeetr97@gmail.com>
2024-03-07 12:20:41 +0530

    7/3/24

---
+++ lib/view/homescreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+class homescreen extends StatelessWidget {
+  const homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+
+       ),
+    );
+  }
+}
