8c0a372c0f56577e1fde7a58078aadb601aa3f83
Meet panchal <panchalmeetr97@gmal.com>
2024-03-11 14:19:46 +0530

    9/3/24

--- lib/view/input_all.dart
+++ lib/view/input_all.dart
@@ -6,6 +6,16 @@ class inputallscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final index = ModalRoute.of(context)?.settings.arguments as int;
+    Future<void> setimage()
+    async {
+      xfile? Image = await imagepicker.pickImage(Source : Imagesource.gallary);
+      GestureDetector(
+        onTap: () {
+
+
+        },
+      );
+    }
     return InputAllScreen(index: index);
   }
 }
@@ -19,6 +29,7 @@ class InputAllScreen extends StatefulWidget {
   State<InputAllScreen> createState() => _InputAllScreenState();
 }
  TextEditingController Txtemail = TextEditingController();
+ Imagepicker imagepicker = Imagepicker();
 
 class _InputAllScreenState extends State<InputAllScreen> {
   @override
@@ -151,10 +162,26 @@ class _InputAllScreenState extends State<InputAllScreen> {
                  width: 361,
                  decoration: BoxDecoration(color: Colors.white),
                ),
-               Container(
-                 height: 800,
-                 width: 361,
-                 decoration: BoxDecoration(color: Colors.white),
+               Padding(
+                 padding: const EdgeInsets.all(20.0),
+                 child: Container(
+                   height: 500,
+                   width: 361,
+                   decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(30)),
+                   child: Column(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.image,color: Color(0XFF6a5ca8),size: 50,),
+                            SizedBox(width: 20,),
+                            Text('Tap to add your \n profile image',style: TextStyle(color: Color(0XFF6a5ca8),fontSize: 30),),
+                          ],
+                        )
+                     ],
+                   ),
+                 ),
                ),
                Container(
                  height: 800,
